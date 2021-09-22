536891205138e7ea705b1a53ec4d5a88cf1429c0
testuser681 <piotrdrabicki99@gmail.com>
2021-09-22 15:43:19 +0200

    trying to create auto scaling

--- ec2.tf
+++ ec2.tf
@@ -23,7 +23,6 @@ resource "aws_instance" "corginstance" {
     private_key = file("~/My_key.pem") 
   }
   
-  
   user_data = <<-EOF
   #!/bin/bash
   sudo su
@@ -37,6 +36,27 @@ resource "aws_instance" "corginstance" {
   tags = {
     Terraform   = "true"
     Environment = "dev"
-    Name = "corginstance"
+    Name        = "corginstance"
   }
+}
+
+resource "aws_ami_from_instance" "corgi_ami" {
+  name               = "corgi-ami"
+  source_instance_id = aws_instance.corginstance.id
+}
+
+resource "aws_launch_configuration" "corgi_config" {
+  //name          = "corgiguration"
+  image_id      = resource.aws_ami_from_instance.corgi_ami.id
+  instance_type = "t2.micro"
+  key_name               = "My_key"
+  iam_instance_profile   = "S3CorgiRole"
+}
+
+resource "aws_autoscaling_group" "corgi_scaling" {
+  //name                 = "corgi-scale"
+  launch_configuration = aws_launch_configuration.corgi_config.name
+  min_size             = 1
+  max_size             = 3
+  availability_zones   = ["us-east-2c"]
 }
