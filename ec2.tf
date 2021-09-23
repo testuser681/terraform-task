00acecd75b642b667c780b8b5ffb77a14d8a98b9
testuser681 <piotrdrabicki99@gmail.com>
2021-09-23 11:46:36 +0200

    launch config working

--- ec2.tf
+++ ec2.tf
@@ -1,27 +1,11 @@
-resource "aws_instance" "corginstance" {
-  ami                    = "ami-00dfe2c7ce89a450b"
+resource "aws_launch_configuration" "corgi_config" {
+  name                   = "corgi-config"
+  image_id               = "ami-00dfe2c7ce89a450b"
   instance_type          = "t2.micro"
   key_name               = "My_key"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-0b5469383489061bf"]
+  enable_monitoring      = true
+  security_groups        = ["sg-0b5469383489061bf"]
   iam_instance_profile   = "S3CorgiRole"
-
-  provisioner "file" {
-    source      = "~/.aws/credentials"
-    destination = "~/.aws/credentials"
-  }
-
-  provisioner "file" {
-    source      = "~/.aws/config"
-    destination = "~/.aws/config"
-  }
-  
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    host        = self.public_ip
-    private_key = file("~/My_key.pem") 
-  }
   
   user_data = <<-EOF
   #!/bin/bash
@@ -32,31 +16,12 @@ resource "aws_instance" "corginstance" {
   systemctl enable httpd.service
   aws s3 cp s3://private-bucket-corgi/corgi.html /var/www/html/index.html
   EOF
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-    Name        = "corginstance"
-  }
-}
-
-resource "aws_ami_from_instance" "corgi_ami" {
-  name               = "corgi-ami"
-  source_instance_id = aws_instance.corginstance.id
-}
-
-resource "aws_launch_configuration" "corgi_config" {
-  //name          = "corgiguration"
-  image_id      = resource.aws_ami_from_instance.corgi_ami.id
-  instance_type = "t2.micro"
-  key_name               = "My_key"
-  iam_instance_profile   = "S3CorgiRole"
 }
 
 resource "aws_autoscaling_group" "corgi_scaling" {
-  //name                 = "corgi-scale"
+  name                 = "corgi-scale"
   launch_configuration = aws_launch_configuration.corgi_config.name
   min_size             = 1
   max_size             = 3
-  availability_zones   = ["us-east-2c"]
+  availability_zones   = ["us-east-2a"]
 }
