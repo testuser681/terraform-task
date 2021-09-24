c69c70e5e7c7df784234d23a85cf68b65fcb4f8e
testuser681 <piotrdrabicki99@gmail.com>
2021-09-24 11:25:29 +0200

    role creating working

--- iam.tf
+++ iam.tf
@@ -1,12 +1,45 @@
-data "aws_iam_policy_document" "read_s3_policy" {
-  statement {
-      actions = ["s3:Get*", "s3:List*"]
-      resources = ["arn:aws:s3:::private-bucket-corgi/*"]
+resource "aws_iam_policy" "read_s3_policy" {
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:Get*",
+                "s3:List*"
+            ],
+            "Resource": "arn:aws:s3:::private-bucket-corgi/*"
+        }
+    ]
   }
+  EOF
 } 
 
 resource "aws_iam_role" "read_s3_role" {
   name = "read-s3-role"
   permissions_boundary = "arn:aws:iam::113304117666:policy/DefaultBoundaryPolicy"
-  assume_role_policy = data.aws_iam_policy_document.read_s3_policy.json
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role = aws_iam_role.read_s3_role.name
+  policy_arn = aws_iam_policy.read_s3_policy.arn
+}
+
+resource "aws_iam_instance_profile" "s3_profile" {
+  name = "s3-profile"
+  role = aws_iam_role.read_s3_role.name
 }
