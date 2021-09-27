35566b1f0fd1ebcd5f66753abc873655d9d468ee
testuser681 <84182009+testuser681@users.noreply.github.com>
2021-09-27 15:24:55 +0200

    aesthetics

--- s3.tf
+++ s3.tf
@@ -10,10 +10,10 @@ resource "aws_s3_bucket" "private_bucket_corgi" {
 
 resource "aws_s3_bucket_object" "corgi" {
   for_each = fileset(".", var.html_name)
-  bucket = aws_s3_bucket.private_bucket_corgi.id
-  key    = each.value
-  acl    = "private"
-  source = "./${each.value}"
-  etag   = filemd5("./${each.value}")
+  bucket   = aws_s3_bucket.private_bucket_corgi.id
+  key      = each.value
+  acl      = "private"
+  source   = "./${each.value}"
+  etag     = filemd5("./${each.value}")
 
 }
